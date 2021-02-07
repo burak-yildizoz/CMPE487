9223c7435931e2872a237070fc667b408391c1ab
rapsodo <alperen.bag@boun.edu.tr>
2021-02-07 14:49:20 +0300

    database initial commit

--- project-i-have-a-question/CommAPI.py
+++ project-i-have-a-question/CommAPI.py
@@ -136,7 +136,6 @@ class CommunicationModule(object):
 
                     
     def _start_tcp_listener(self):
-        """ Listens MESSAGE and RESPOND packets """
         quit_listener = False
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
--- project-i-have-a-question/database.py
+++ project-i-have-a-question/database.py
@@ -1,6 +1,30 @@
-
+from question import *
 
 class Database(object):
 
     def __init__(self):
-        self.questions = [] # type: List[Question]
+        self.questions = {} # type: Dict{str -> Question}
+
+    def update_database(self, packet):
+        # packet is already validated 
+        if packet["TYPE"] == "QUESTION":
+            # TODO username instead of ip
+            new_question = Question(packet["ACTOR"], packet["TITLE"], packet["CONTENT"])
+            
+            if not (packet["TITLE"] in self.questions):
+                self.questions[packet["TITLE"]] = new_question
+
+        elif packet["TYPE"] == "ANSWER":
+            if packet["QUESTION_TITLE"] in self.questions:
+                answer_object = Answer(packet["ACTOR"], packet["CONTENT"])
+                self.questions[packet["QUESTION_TITLE"]].answer(answer_object)
+
+        elif packet["TYPE"] == "VOTE":
+            assert packet["VOTE"] == "+" or packet["VOTE"] == "-"
+            question_title = packet["QUESTION_TITLE"]
+            if question_title in self.questions:
+                if packet["VOTE"] == "+":
+                    self.questions[question_title].upvote(packet["ACTOR"])
+                elif packet["VOTE"] == "-":
+                    self.questions[question_title].downvote(packet["ACTOR"])
+
